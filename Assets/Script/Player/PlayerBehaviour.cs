4b0540faa1f2e9ee8f5f7b99817fe9462082ca3b
Romain <mromain911@gmail.com>
2022-09-16 15:41:52 +0200

    Animation Player

--- Assets/Script/Player/PlayerBehaviour.cs
+++ Assets/Script/Player/PlayerBehaviour.cs
@@ -4,80 +4,77 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    [Header("Movement")]
-    public float speed = 200.0f;
+    [Header("Movement")] public float speed = 200.0f;
     public float limiter = 0.7f;
     private bool flip = false;
- 
+
     private Vector2 movement;
-    
+
+    [Header("Component")]
     private Rigidbody2D rb;
     private CapsuleCollider2D playerCollider;
     private SpriteRenderer sprite;
+    private Animator anim;
 
     [SerializeField] private LayerMask wallLayer;
     [SerializeField] private float range;
     private Vector2 dir;
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<CapsuleCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    
+
     private void FixedUpdate()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        
+
         if (movement.x != 0 && movement.y != 0)
         {
             movement.x *= limiter;
             movement.y *= limiter;
         }
-        
-        
-        rb.velocity = new Vector2(movement.x * speed * Time.deltaTime, movement.y* speed * Time.deltaTime);
 
-        Flip(rb.velocity.x);
+        bool isIdle = movement.x == 0 && movement.y == 0;
+        if (isIdle)
+        {
+            //Idle
+            rb.velocity = Vector2.zero;
+            anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            //Moving
+            rb.velocity = new Vector2(movement.x * speed * Time.deltaTime, movement.y * speed * Time.deltaTime);
+            anim.SetFloat("Xinput", movement.x);
+            anim.SetFloat("Yinput", movement.y);
+            anim.SetBool("isMoving", true);
+        }
+
+
+
+        if (movement.x > 0) dir = Vector2.right;
+        else if (movement.x < 0) dir = Vector2.left;
+        if (movement.y > 0) dir = Vector2.up;
+        else if (movement.y < 0) dir = Vector2.down;
 
         RaycastHit2D yes = Physics2D.Raycast(this.transform.position, dir, range, wallLayer);
-        if(yes.collider != null)
+        if (yes.collider != null)
         {
             rb.velocity = Vector2.zero;
         }
+
+
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawRay(this.transform.position, dir * range);
     }
-
-    private void Flip(float _velocity)
-    {
-        if (_velocity > 0.2f)
-        {
-            sprite.flipX = false;
-            if (flip == false)
-            {
-                flip = true;
-            }
-            
-        }
-        else if (_velocity < -0.2f)
-        {
-            sprite.flipX = true;
-            if (flip == true)
-            {
-                flip = false;
-            }
-            
-        }
-            if(movement.x > 0)dir = Vector2.right;
-            else if(movement.x < 0)dir = Vector2.left;
-            if(movement.y > 0)dir = Vector2.up;
-            else if(movement.y < 0)dir = Vector2.down;
-    }
 }
+    
