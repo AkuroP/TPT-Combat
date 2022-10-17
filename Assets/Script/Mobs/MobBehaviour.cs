b41c6c2227d95c160946373e21f17a7ad471e3b4
Romain <mromain911@gmail.com>
2022-10-17 17:21:10 +0200

    Mob Random Move

--- Assets/Script/Mobs/MobBehaviour.cs
+++ Assets/Script/Mobs/MobBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MobBehaviour : MonoBehaviour
@@ -19,12 +20,34 @@ public class MobBehaviour : MonoBehaviour
     public Animator animator;
     public GameObject fightScene;
     public GameObject transitionObject;
+    
+    private Vector2 direction;
+    
+    public float radius  = 40.0f;
+
+    // The point we are going around in circles
+    private Vector2 basestartpoint;
+
+    // Destination of our current move
+    private Vector2 destination;
+
+    // Start of our current move
+    private Vector2 start;
+
+    // Current move's progress
+    private float progress = 0.0f;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
+        
+        start = transform.localPosition;
+        basestartpoint = transform.localPosition;
+        progress = 0.0f;
+
+        PickNewRandomDestination();
     }
     void Update()
     {
@@ -51,8 +74,41 @@ public class MobBehaviour : MonoBehaviour
             mov = MPdir;
             rb.velocity = new Vector2(mov.x, mov.y) * speed;
         }
+        else
+        {
+            bool reached = false;
+
+            // Update our progress to our destination
+            progress += speed * Time.deltaTime;
+
+            // Check for the case when we overshoot or reach our destination
+            if (progress >= 1.0f)
+            {
+                progress = 1.0f;
+                reached = true;
+            }
+
+            // Update out position based on our start postion, destination and progress.
+            transform.localPosition = (destination * progress) + start * (1 - progress);
+
+            // If we have reached the destination, set it as the new start and pick a new random point. Reset the progress
+            if (reached)
+            {
+                start = destination;
+                PickNewRandomDestination();
+                progress = 0.0f;
+            }
+            
+        }
         
     }
+    
+    void PickNewRandomDestination()
+    {
+        // We add basestartpoint to the mix so that is doesn't go around a circle in the middle of the scene.
+        destination = Random.insideUnitCircle * radius + basestartpoint;
+    }
+
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -80,9 +136,14 @@ public class MobBehaviour : MonoBehaviour
         transitionObject.SetActive(false);
         
     }
-    
-    void OnDrawGizmos()
+    #if UNITY_EDITOR
+    void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(this.transform.position, mobRadius);
+        UnityEditor.Handles.color = Color.red;
+        UnityEditor.Handles.DrawWireDisc(transform.position, new Vector3(0, 0,1 ), mobRadius);
+        
+        UnityEditor.Handles.color = Color.yellow;
+        UnityEditor.Handles.DrawWireDisc(Random.insideUnitCircle + basestartpoint, new Vector3(0, 0,1), radius);
     }
+    #endif
 }
