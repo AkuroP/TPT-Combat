66077bc81e1b74e95f40d381d9a4dbdaf80de7dd
tmoryas <56356128+tmoryas@users.noreply.github.com>
2022-10-17 14:03:32 +0200

    chara creation tool 0

--- Assets/Script/Tools/CharacterCreationWindow.cs
+++ Assets/Script/Tools/CharacterCreationWindow.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class ToolboxWindow : EditorWindow
+public class CharacterCreationWindow : EditorWindow
 {
     [MenuItem("Toolbox/Toolbox Window")]
     static void InitWindow()
     {
-        ToolboxWindow window = GetWindow<ToolboxWindow>();
-        window.titleContent = new GUIContent("Toolbox");
+        CharacterCreationWindow window = GetWindow<CharacterCreationWindow>();
+        window.titleContent = new GUIContent("Create character");
         window.Show();
     }
 
     void OnGUI()
     {
+        GUI.backgroundColor = new Color(0.8f, 0.4f, 0, 1);
+
         if (GUILayout.Button("bouton"))
         {
             Debug.Log("oui");
