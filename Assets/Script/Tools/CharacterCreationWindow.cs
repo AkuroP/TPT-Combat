61fd10cf2c1bf4830fe357f78be884377579ad5e
tmoryas <56356128+tmoryas@users.noreply.github.com>
2022-11-02 09:40:27 +0100

    push

--- Assets/Script/Tools/CharacterCreationWindow.cs
+++ Assets/Script/Tools/CharacterCreationWindow.cs
@@ -5,21 +5,71 @@ using UnityEditor;
 
 public class CharacterCreationWindow : EditorWindow
 {
-    [MenuItem("Toolbox/Toolbox Window")]
+    SerializedObject serializedObject;
+    SerializedProperty currentProperty;
+    Object entityData;
+    
+
+    [MenuItem("Toolbox/Character Creation")]
     static void InitWindow()
     {
         CharacterCreationWindow window = GetWindow<CharacterCreationWindow>();
         window.titleContent = new GUIContent("Create character");
+        //window.serializedObject = new SerializedObject();
         window.Show();
+    }
+
+    public Vector2 scrollPosition = Vector2.zero;
+
+    void Start()
+    {
+
     }
 
     void OnGUI()
     {
-        GUI.backgroundColor = new Color(0.8f, 0.4f, 0, 1);
+        GUI.backgroundColor = new Color(1f, .8f, 0.8f, 1);
 
-        if (GUILayout.Button("bouton"))
+        // if (GUILayout.Button("bouton"))
+        // {
+        //     Debug.Log("oui");
+        // }
+
+        // scrollPosition = GUI.BeginScrollView(new Rect(10, 10, 300, 400), scrollPosition, new Rect (0, 0, 220, 200));
+        // GUI.Label(new Rect(5, 10, 50, 20), "Name");
+        // GUI.TextField(new Rect(5, 30, 100, 20), "");
+        
+        entityData = EditorGUILayout.ObjectField(entityData, typeof(SerializedObject), true);
+        currentProperty = serializedObject.FindProperty("entityData");
+        DrawProperties(currentProperty, true);
+        
+        GUI.EndScrollView();
+    }
+
+    void DrawProperties(SerializedProperty prop, bool drawChildren)
+    {
+        string lastPropPath = string.Empty;
+        foreach(SerializedProperty p in prop)
         {
-            Debug.Log("oui");
+            if (p.isArray && p.propertyType == SerializedPropertyType.Generic)
+            {
+                EditorGUILayout.BeginHorizontal();
+                p.isExpanded = EditorGUILayout.Foldout(p.isExpanded, p.displayName);
+                EditorGUILayout.EndHorizontal();
+
+                if (p.isExpanded)
+                {
+                    EditorGUI.indentLevel++;
+                    DrawProperties(p, drawChildren);
+                    EditorGUI.indentLevel--;
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(lastPropPath) && p.propertyPath.Contains(lastPropPath)){continue;}
+                lastPropPath = p.propertyPath;
+                EditorGUILayout.PropertyField(p, drawChildren);
+            }
         }
     }
 }
