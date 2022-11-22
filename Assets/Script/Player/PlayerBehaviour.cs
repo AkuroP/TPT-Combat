476c83303f8646d6de3c6cf62dddba3249ac3071
tmoryas <56356128+tmoryas@users.noreply.github.com>
2022-11-22 15:06:36 +0100

    run particles

--- Assets/Script/Player/PlayerBehaviour.cs
+++ Assets/Script/Player/PlayerBehaviour.cs
@@ -22,6 +22,11 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField] private LayerMask wallLayer;
     [SerializeField] private float range;
     private Vector2 dir;
+
+    [SerializeField] private GameObject runParticles;
+    [SerializeField] private float particlesTimerMax = 0.6f;
+    private float particlesTimer;
+    bool particleStop;
     
 
     void Start()
@@ -34,6 +39,8 @@ public class PlayerBehaviour : MonoBehaviour
 
         speedSave = moveSpeed;
 
+        runParticles.SetActive(false);
+        particlesTimer = 0;
     }
 
 
@@ -54,6 +61,11 @@ public class PlayerBehaviour : MonoBehaviour
             //Idle
             rb.velocity = Vector2.zero;
             anim.SetBool("isMoving", false);
+            
+            
+            if (particlesTimer > 0)particlesTimer = Mathf.Clamp(particlesTimer - Time.fixedDeltaTime, 0, particlesTimerMax);
+            else runParticles.SetActive(false);
+
         }
         else
         {
@@ -63,6 +75,7 @@ public class PlayerBehaviour : MonoBehaviour
             anim.SetFloat("Yinput", movement.y);
             anim.SetBool("isMoving", true);
             dir = new Vector2(movement.x, movement.y);
+            runParticles.SetActive(true);
         }
 
         RaycastHit2D yes = Physics2D.Raycast((Vector2)this.transform.position, dir, range, wallLayer);
@@ -92,9 +105,14 @@ public class PlayerBehaviour : MonoBehaviour
         }
     }
 
-    void OnMove(InputValue v)
+    public void OnMove(InputAction.CallbackContext ctx)
     {
-        movement = v.Get<Vector2>();
+        movement = ctx.ReadValue<Vector2>();
+
+        if (ctx.canceled)
+        {
+            particlesTimer = particlesTimerMax;
+        }
     }
 
     private void OnDrawGizmos()
